26b9258dd2de07d3d1eaf17034352e46f8c858ce
Shreyash <shreyash.p@hubengage.com>
2025-05-02 06:39:27 +0530

    List

--- app/src/main/java/com/shreyash/simplecompose/MainActivity.kt
+++ app/src/main/java/com/shreyash/simplecompose/MainActivity.kt
@@ -1,30 +1,48 @@
 package com.shreyash.simplecompose
 
+import Message
+import SampleData
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.shreyash.simplecompose.ui.theme.SimpleComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,16 +51,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimpleComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MessageCard(
-                        Message("Shreyash", "Android dev")
-                    )
-                }
+                TransparentSystemBars()
+                    Conversation(SampleData.conversationSample)
             }
         }
     }
 }
-data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
@@ -57,7 +71,15 @@ fun MessageCard(msg: Message) {
         )
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
@@ -66,24 +88,54 @@ fun MessageCard(msg: Message) {
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(
+                color = surfaceColor,
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
         }
     }
 }
-
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Surface {
-        MessageCard(
-            msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-        )
+fun TransparentSystemBars() {
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+
+    SideEffect {
+        window.statusBarColor = Color.Transparent.toArgb()
+        window.navigationBarColor = Color.Transparent.toArgb()
     }
 }
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(WindowInsets.systemBars.asPaddingValues())
+    ){
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+
+@Preview (showBackground = true)
+@Composable
+fun PreviewConversation() {
+    SimpleComposeTheme  {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+
